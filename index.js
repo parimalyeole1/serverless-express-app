2fc6c31105d20c986370309d7c9edeba919c6257
Parimal Yeole <parimal.yeole1@gmail.com>
2018-02-16 19:07:27 +0530

    local testing dynamodb added

--- index.js
+++ index.js
@@ -7,7 +7,17 @@ const AWS = require('aws-sdk');
 
 
 const USERS_TABLE = process.env.USERS_TABLE;
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const IS_OFFLINE = process.env.IS_OFFLINE;
+let dynamoDb;
+if (IS_OFFLINE === 'true') {
+    dynamoDb = new AWS.DynamoDB.DocumentClient({
+        region: 'localhost',
+        endpoint: 'http://localhost:8000'
+    })
+    console.log(dynamoDb);
+} else {
+    dynamoDb = new AWS.DynamoDB.DocumentClient();
+};
 
 app.use(bodyParser.json({ strict: false }));
 
@@ -18,25 +28,25 @@ app.get('/', function (req, res) {
 
 app.get('/users/:userId', function (req, res) {
     const params = {
-      TableName: USERS_TABLE,
-      Key: {
-        userId: req.params.userId,
-      },
+        TableName: USERS_TABLE,
+        Key: {
+            userId: req.params.userId,
+        },
     }
-  
+
     dynamoDb.get(params, (error, result) => {
-      if (error) {
-        console.log(error);
-        res.status(400).json({ error: 'Could not get user' });
-      }
-      if (result.Item) {
-        const {userId, name} = result.Item;
-        res.json({ userId, name });
-      } else {
-        res.status(404).json({ error: "User not found" });
-      }
+        if (error) {
+            console.log(error);
+            res.status(400).json({ error: 'Could not get user' });
+        }
+        if (result.Item) {
+            const { userId, name } = result.Item;
+            res.json({ userId, name });
+        } else {
+            res.status(404).json({ error: "User not found" });
+        }
     });
-  })
+})
 
 // Create User endpoint
 app.post('/users', function (req, res) {
